f567b55e6cd865f18c863821b35e2ef5b247ccec
juanmiloz <juanmiloz@hotmail.com>
2025-01-06 13:24:34 -0500

    base commit - for items microservice

---
+++ src/main/java/com/microcore/springcloud/msvc/items/MsvcItemsApplication.java
@@ -0,0 +1,13 @@
+package com.microcore.springcloud.msvc.items;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcItemsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsvcItemsApplication.class, args);
+    }
+
+}
