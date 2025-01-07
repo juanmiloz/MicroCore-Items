dbc5e273737223efa1675826d641df25f945ca1e
juanmiloz <juanmiloz@hotmail.com>
2025-01-06 22:22:35 -0500

    fix scope of the variable

---
+++ src/main/java/com/microcore/springcloud/msvc/items/controllers/ItemController.java
@@ -0,0 +1,36 @@
+package com.microcore.springcloud.msvc.items.controllers;
+
+import com.microcore.springcloud.msvc.items.models.Item;
+import com.microcore.springcloud.msvc.items.services.ItemService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@AllArgsConstructor
+public class ItemController {
+
+    final private ItemService itemService;
+
+    @GetMapping
+    public ResponseEntity<List<Item>> list() {
+        return ResponseEntity.ok(itemService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Item> details(@PathVariable Long id) {
+        Optional<Item> itemOptional = itemService.findById(id);
+
+        if(itemOptional.isPresent()){
+            return ResponseEntity.ok(itemOptional.get());
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+}
