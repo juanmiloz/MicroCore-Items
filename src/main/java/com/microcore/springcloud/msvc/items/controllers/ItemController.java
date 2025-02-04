01b672501fd16f4747a8f9a618a8d2becf706aec
juanmiloz <juanmiloz@hotmail.com>
2025-02-04 01:18:42 -0500

    .\feat: add CircuitBreaker and logging in ItemController, comment exception handling in ItemServiceFeign
    - Added CircuitBreakerFactory and Logger in ItemController
    - Implemented CircuitBreaker pattern for indById with fallback mechanism
    - Commented out exception handling (	ry-catch) in indById method of ItemServiceFeign

--- src/main/java/com/microcore/springcloud/msvc/items/controllers/ItemController.java
+++ src/main/java/com/microcore/springcloud/msvc/items/controllers/ItemController.java
@@ -1,16 +1,16 @@
 package com.microcore.springcloud.msvc.items.controllers;
 
 import com.microcore.springcloud.msvc.items.models.Item;
+import com.microcore.springcloud.msvc.items.models.Product;
 import com.microcore.springcloud.msvc.items.services.ItemService;
-import com.microcore.springcloud.msvc.items.services.ItemServiceFeign;
-import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -18,21 +18,42 @@ import java.util.Optional;
 @RestController
 public class ItemController {
 
-    final private ItemService itemService;
+    private final Logger logger = LoggerFactory.getLogger(ItemController.class);
+    private final ItemService itemService;
+    private final CircuitBreakerFactory cBreakerFactory;
 
-    public ItemController(@Qualifier("itemServiceFeign") ItemService itemService) {
+    public ItemController(@Qualifier("itemServiceFeign") ItemService itemService, CircuitBreakerFactory cBreakerFactory) {
         this.itemService = itemService;
+        this.cBreakerFactory = cBreakerFactory;
     }
 
     @GetMapping
-    public ResponseEntity<List<Item>> list() {
+    public ResponseEntity<List<Item>> list(
+            @RequestParam(name = "name", required = false) String name,
+            @RequestHeader(name = "token-request", required = false) String token
+    )
+    {
+        System.out.println(name); //To recover the request parameter from gateway filter
+        System.out.println(token);//To recover the request header from gateway filter
         return ResponseEntity.ok(itemService.findAll());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> details(@PathVariable Long id) {
-        Optional<Item> itemOptional = itemService.findById(id);
+    public ResponseEntity<?> details(@PathVariable Long id) throws InterruptedException {
+        Optional<Item> itemOptional = cBreakerFactory.create("items").run(
+                () -> itemService.findById(id),
+                e -> {
+                    System.out.println(e.getMessage());
+                    logger.error(e.getMessage());
 
+                    Product product = new Product();
+                    product.setCreateAt(LocalDate.now());
+                    product.setId(1L);
+                    product.setName("Camara Sony");
+                    product.setPrice(500.00);
+                    return Optional.of(new Item(product, 5));
+                }
+        );
         if(itemOptional.isPresent()){
             return ResponseEntity.ok(itemOptional.get());
         }else{
--- src/main/java/com/microcore/springcloud/msvc/items/services/ItemServiceFeign.java
+++ src/main/java/com/microcore/springcloud/msvc/items/services/ItemServiceFeign.java
@@ -31,11 +31,11 @@ public class ItemServiceFeign implements ItemService {
 
     @Override
     public Optional<Item> findById(Long id) {
-        try{
+//        try{
             Product product = client.details(id);
             return Optional.of(new Item(product, new Random().nextInt(10)+1));
-        }catch (FeignException e){
-            return Optional.empty();
-        }
+//        }catch (FeignException e){
+//            return Optional.empty();
+//        }
     }
 }
